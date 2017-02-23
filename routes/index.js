61353cf907f626cf102015687316b475d1e27aff
Priyanka Dasgupta <pdasgupt@ucsd.edu>
2017-02-22 20:38:55 -0800

    initializedPage function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
 exports.view = function(req, res){
-  	res.render('index', projects);
-};
+
+ 	var randomNum = Math.random();
+ 	console.log(randomNum);
+ 	if (randomNum > 0.5) {
+ 		projects['grid'] = false;
+ 		res.render('index', projects);
+ 	} else {
+ 		res.redirect('/grid');
+ 	}
+ };
+
+ exports.viewGrid = function(req, res){
+ 	projects["grid"] = true;
+ 	res.render('index', projects);
+ };
